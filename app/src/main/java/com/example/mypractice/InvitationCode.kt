da53cb469d5929e195152284641b398489603814
JayWoodroffe <jenawoodroffe@gmail.com>
2024-04-18 20:56:43 +0200

    invitations being sent nicely

--- app/src/main/java/com/example/mypractice/InvitationCode.kt
+++ app/src/main/java/com/example/mypractice/InvitationCode.kt
@@ -1,11 +1,15 @@
 package com.example.mypractice
 
+import android.content.ActivityNotFoundException
 import android.content.ClipData
 import android.content.ClipboardManager
 import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Html
 import android.view.View
+import android.widget.Toast
 import com.example.mypractice.data.ClientDataAccess
 import com.example.mypractice.data.DoctorDataHolder
 import com.example.mypractice.databinding.ActivityInvitationCodeBinding
@@ -14,6 +18,7 @@ import java.util.Calendar
 class InvitationCode : AppCompatActivity() {
 
     private lateinit var binding: ActivityInvitationCodeBinding
+    private lateinit var email:String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,23 +31,54 @@ class InvitationCode : AppCompatActivity() {
                         View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
                 )
 
-        var code = displayNewCode()
 
-        binding.btnCopy.setOnClickListener {
-            copyToClipboard(code)
-            addCodeToDb(code)
-        }
 
         binding.btnNewCode.setOnClickListener {
-            code = displayNewCode()
+            if (checkEmail())
+                formatEmail();
+            else
+                Toast.makeText(this, "Incorrect email format", Toast.LENGTH_SHORT).show()
         }
 
     }
 
-    private fun displayNewCode():String {
-        val code = generateCode()
-        binding.tvCode.text = code
-        return code
+    private fun checkEmail(): Boolean
+    {
+        email = binding.etClientEmail.text.toString();
+        email = email.trim();
+        if (email.isNullOrEmpty()) return false
+        val emailRegex = Regex("[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}")
+        return emailRegex.matches(email)
+    }
+
+    private fun formatEmail()
+    {
+        val doctorsName = DoctorDataHolder.getLoggedInDoctor()?.name;
+        val invitationCode = generateCode()
+        val subject = "PhysioLink Invitation"
+
+        val body = "Your physiotherapist, Dr. $doctorsName, has invited you to download and register for the application PhysioLink." +
+                "\n" +
+                "Invitation code: $invitationCode" +
+                "\nEnter this code into the application to register and access important exercise " +
+                "information from your doctor." +
+                "\n" +
+                "Welcome to the team,\nThe PhysioLink Team"
+
+        val intent = Intent(Intent.ACTION_SEND)
+        intent.type = "text/plain"
+        intent.putExtra(Intent.EXTRA_EMAIL, arrayOf(email))
+        intent.putExtra(Intent.EXTRA_SUBJECT, subject)
+        intent.putExtra(Intent.EXTRA_TEXT, Html.fromHtml(body))
+        intent.setPackage("com.google.android.gm") // Specify package to open Gmail app
+
+        try {
+            addCodeToDb(invitationCode)
+            startActivity(intent)
+        } catch (e: ActivityNotFoundException) {
+            // Gmail app is not installed, handle the error or prompt the user to install it
+            Toast.makeText(this, "Gmail app is not installed", Toast.LENGTH_SHORT).show()
+        }
     }
 
     fun generateCode():String{
